6c270c5dab1754d38757f4425226e34c83516984
yogeshj56@gmail.com <yogeshj56@gmail.com>
2020-07-02 15:04:16 +0530

    MenuComponent part-1 assignment

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Menu from './Components/MenuComponent';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color = "primary">
         <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>   
         </div>
       </Navbar>
+
+      <Menu/>
+
     </div>
+    
   );
 }
 
