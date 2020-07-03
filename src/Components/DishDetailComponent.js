6ef8da0a2a52f21a7b3606755ccf0b04f39e98c0
yogeshj56@gmail.com <yogeshj56@gmail.com>
2020-07-03 10:47:11 +0530

    updated code by separating Presentational and Container components

--- src/Components/DishDetailComponent.js
+++ src/Components/DishDetailComponent.js
@@ -2,13 +2,6 @@ import React, { Component } from 'react';
 import { Card, CardBody, CardImg, CardText, CardTitle } from 'reactstrap';
 
 class DishDetailComponent extends Component{
-    constructor(props){
-        super(props);
-
-        this.state = {
-            
-        }
-    }
 
     renderComments(){
         var commentArr = this.props.selectedDish.comments;
@@ -16,7 +9,9 @@ class DishDetailComponent extends Component{
 
             const comments = commentArr.map((commentDetails)=> {    
                 return(
-                <div key={commentDetails.id}><br/> {commentDetails.comment} <br/> -- {commentDetails.author}   {commentDetails.date}</div>  
+                <div key={commentDetails.id}><br/> {commentDetails.comment}
+                     <br/> -- {commentDetails.author}    
+                     {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short',day:'2-digit'}).format(new Date(Date.parse(commentDetails.date)))}</div>  
                 )              
             });
             
@@ -38,19 +33,24 @@ class DishDetailComponent extends Component{
         if(this.props.selectedDish != null){
             var dish = this.props.selectedDish
             return(
-                <div className="row">
+                
+                
+                    <div className="container">
+                    <div className="row">
                     <div className="col-12 col-md-5 m-1">
                         <Card>
+                        
                             <CardImg width='100%' src={dish.image} alt={dish.name}/>
                             <CardBody>
                                 <CardTitle>{dish.name}</CardTitle>
                                 <CardText>{dish.description}</CardText>
                             </CardBody>
+                           
                         </Card>
-                    </div>
-                    
+                        </div>
+                   
                     {this.renderComments()}
-                    
+                    </div>
                 </div>
             );
         }
--- src/Components/MenuComponent.js
+++ src/Components/MenuComponent.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
-import DishDetailComponent from './DishDetailComponent';
 
 class Menu extends Component {
     constructor(props) {
         super(props);
-
-        this.state = {
-            selectedDish: null
-        };
 
         console.log('Menu component constructor is invoked');
     }
@@ -17,15 +12,11 @@ class Menu extends Component {
         console.log('Menu component componentDidMount is invoked');
     }
 
-    onDishSelect(dish){
-        this.setState({ selectedDish:dish })
-    }
-
     render() {
         const menu = this.props.dishes.map((dish)=> {
             return(
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card onClick={() => this.onDishSelect(dish)}>
+                    <Card onClick={()=> this.props.onClick(dish.id)}>
                         <CardImg width='100%' src={dish.image} alt={dish.name}/>
 
                         <CardImgOverlay>
@@ -40,11 +31,10 @@ class Menu extends Component {
 
         return(
             <div className="container">
+                
                 <div className="row">
                     {menu}
                 </div>
-
-                <DishDetailComponent selectedDish={this.state.selectedDish} />         
             
             </div>
         );
